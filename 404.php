6a9619b40ce7aa180eec405f3ede7e2552ea9099
a.almayhob <a.almayhob@haykalmedia.com>
2023-07-01 21:50:17 +0300

    404 Page

--- 404.php
+++ 404.php
@@ -4,23 +4,8 @@ get_header()
 ?>
 <main id="particles-js" class="particles-js">
     <div class="content">
-        <h1 class="title">لم يتم العثور على الصفحة 😐</h1>
-        <h2 class="sub-title">استمتع بتأثير الparticles بدلاً عن ذلك 😁</h2>
-        <a href="<?=SITE_URL?>" class="back-to-home">عودة إلى ميسان</a>
+        <h1 class="title"><?= __("Lost in space. No where to go <br>Don't scream, Nobody can hear you",'abdoo') ?></h1>
+        <a href="<?=SITE_URL?>" class="back-to-home"><?= __('Go back to Abdoo','abdoo')?></a>
     </div>
-    <!-- div class="seperator"></div -->
-    <div class="container-fluid py-5 latest-articles">
-        <div class="row pb-4">
-            <h2>أو تصفح آخر مقالاتنا</h2>
-        </div>
-        <div class="row">
-        <? $latest_posts = abdoo_get_latest_posts(); ?>
-        <? while (  $latest_posts->have_posts() ):  $latest_posts->the_post();?>
-            <article class="col-lg-2 col-md-4">
-                <? get_template_part('template-parts/loop','vert') ?>
-            </article>
-        <? endwhile ?>
-        </div>
-    </div -->  
 </main>
 <? get_footer() ?>
